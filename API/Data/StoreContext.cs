fb70278f5d184346e5f8d3e96c1776d311ebf20d
bohdanrubell <bohdanrubell@gmail.com>
2024-07-09 16:25:43 +0300

    [Backend] Start creating ordering. Added Order entity Recreated database for use integer id's anywhere

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -1,23 +1,32 @@
 using API.Entities;
+using API.Entities.OrderAggregate;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
-public class StoreContext(DbContextOptions options) : IdentityDbContext<User>(options)
+public class StoreContext(DbContextOptions options) : IdentityDbContext<User, Role, int>(options)
 {
     public DbSet<Product> Products { get; set; }
     public DbSet<Basket> Baskets { get; set; }
+
+    public DbSet<Order> Orders { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
 
-        builder.Entity<IdentityRole>()
+        builder.Entity<User>()
+            .HasOne(a => a.Address)
+            .WithOne()
+            .HasForeignKey<UserAddress>(a => a.Id)
+            .OnDelete(DeleteBehavior.Cascade);
+        
+        builder.Entity<Role>()
             .HasData(
-                new IdentityRole { Name = "Member", NormalizedName = "MEMBER" },
-                new IdentityRole { Name = "Admin", NormalizedName = "ADMIN" }
+                new Role { Id = 2, Name = "Member", NormalizedName = "MEMBER" },
+                new Role { Id = 1, Name = "Admin", NormalizedName = "ADMIN" }
             );
     }
 }
