36e571efb289ce944f73a2881800908204deae80
bohdanrubell <bohdanrubell@gmail.com>
2024-07-09 16:24:31 +0300

    [Frontend] Added possibility for requiring to login in the pages which added to private route

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -6,7 +6,7 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import {createTheme,ThemeProvider } from '@mui/material/styles';
 import {Box, Paper} from "@mui/material";
-import {Link, useNavigate} from "react-router-dom";
+import {Link, useLocation, useNavigate} from "react-router-dom";
 import {FieldValues, useForm} from "react-hook-form";
 import {LoadingButton} from "@mui/lab";
 import {useAppDispatch} from "../../app/store/configureStore.ts";
@@ -19,6 +19,7 @@ const defaultTheme = createTheme();
 export default function Login() {
 
     const navigate = useNavigate();
+    const location = useLocation();
     const dispatch = useAppDispatch();
     const {register,
         handleSubmit,
@@ -29,7 +30,7 @@ export default function Login() {
     async function submitForm(data: FieldValues) {
         try{
         await dispatch(signInUser(data));
-        navigate('/catalog');
+        navigate(location.state?.from || '/catalog');
         } catch (error) {
             console.log(error);
         }
