fb9ab84322355e08b4362f6943425f4f15efe709
bohdanrubell <bohdanrubell@gmail.com>
2024-07-10 15:41:24 +0300

    [Frontend] Added validation for the payment form(all values will be added in the near future)

--- client/src/features/checkout/checkoutValidation.ts
+++ client/src/features/checkout/checkoutValidation.ts
@@ -1,7 +1,8 @@
 import * as yup from "yup";
 
 
-export const validationSchema = yup.object({
+export const validationSchema = [
+    yup.object({
     fullName: yup.string().required('Full name is required'),
     address1: yup.string().required('Address line 1 is required'),
     address2: yup.string().required('Address line 2 is required'),
@@ -9,4 +10,9 @@ export const validationSchema = yup.object({
     zip: yup.string().required('Zip is required'),
     state: yup.string().required('State is required'),
     country: yup.string().required('Country is required'),
-});
+    }),
+    yup.object(),
+    yup.object({
+        nameOnCard: yup.string().required('Name on card is required')
+    })
+]
