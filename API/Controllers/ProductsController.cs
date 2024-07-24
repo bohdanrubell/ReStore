8285fc2749b3a83110d0fc8ff9ee959d9f87c379
bohdanrubell <bohdanrubell@gmail.com>
2024-07-24 18:45:24 +0300

    [Backend] Implementing admin's inventory
    Added automapper
    Connected with Cloudinary for store product's photos
    Create&UpdateDtoo for Mapper

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,13 +1,17 @@
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using API.Extensions;
 using API.RequestHelpers;
+using API.Services;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class ProductsController(StoreContext context) : BaseApiController
+public class ProductsController(StoreContext context, IMapper mapper, ImageService imageService) : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<PagedList<Product>>> GetProducts([FromQuery] ProductParams productParams)
@@ -26,7 +30,7 @@ public class ProductsController(StoreContext context) : BaseApiController
         return products;
     }
 
-    [HttpGet("{id}")]
+    [HttpGet("{id}", Name = "GetProduct")]
     public async Task<ActionResult<Product>> GetProduct(int id)
     {
         var product = await context.Products.FindAsync(id);
@@ -44,4 +48,81 @@ public class ProductsController(StoreContext context) : BaseApiController
 
         return Ok(new { brands, types });
     }
+
+    [Authorize(Roles = "Admin")]
+    [HttpPost]
+    public async Task<ActionResult<Product>> CreateProduct([FromForm]CreateProductDto productDto)
+    {
+        var product = mapper.Map<Product>(productDto);
+
+        if (productDto.File is not null)
+        {
+            var imageResult = await imageService.AddImageAsync(productDto.File);
+
+            if (imageResult.Error is not null)
+                return BadRequest(new ProblemDetails { Title = imageResult.Error.Message });
+
+            product.PictureUrl = imageResult.SecureUrl.ToString();
+            product.PublicId = imageResult.PublicId;
+        }
+        
+        context.Products.Add(product);
+
+        var result = await context.SaveChangesAsync() > 0;
+
+        if (result) return CreatedAtRoute("GetProduct", new { Id = product.Id }, product);
+
+        return BadRequest(new ProblemDetails { Title = "Problem creating a new product" });
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpPut]
+    public async Task<ActionResult<Product>> UpdateProduct([FromForm]UpdateProductDto productDto)
+    {
+        var product = await context.Products.FindAsync(productDto.Id);
+        if (product is null) return NotFound();
+
+        mapper.Map(productDto, product);
+
+        if (productDto.File is not null)
+        {
+            var imageResult = await imageService.AddImageAsync(productDto.File);
+            
+            if (imageResult.Error is not null)
+                return BadRequest(new ProblemDetails { Title = imageResult.Error.Message });
+
+            if (!string.IsNullOrEmpty(product.PublicId)) 
+                await imageService.DeleteImageAsync(product.PublicId);
+            
+            product.PictureUrl = imageResult.SecureUrl.ToString();
+            product.PublicId = imageResult.PublicId;
+        }
+        
+        var result = await context.SaveChangesAsync() > 0;
+
+        if (result) return Ok(product);
+
+        return BadRequest(new ProblemDetails { Title = "The problem was occured by update product" });
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpDelete]
+    public async Task<ActionResult> DeleteProduct(int id)
+    {
+        var product = await context.Products.FindAsync(id);
+        
+        if (product is null) return NotFound();
+        
+        if (!string.IsNullOrEmpty(product.PublicId)) 
+            await imageService.DeleteImageAsync(product.PublicId);
+        
+        context.Products.Remove(product);
+        
+        var result = await context.SaveChangesAsync() > 0;
+
+        if (result) return Ok();
+
+        return BadRequest(new ProblemDetails { Title = "The problem was occured by delete product" });
+    }
+    
 }
