5cf63d4c4426d95c622242cf0792f5ca5b26b482
bohdanrubell <bohdanrubell@gmail.com>
2025-01-09 22:50:48 +0200

    feat: configure dependencies and clean some in file

--- API/Program.cs
+++ API/Program.cs
@@ -4,6 +4,7 @@ using API.Entities;
 using API.Middleware;
 using API.RequestHelpers;
 using API.Services;
+using Carter;
 using FluentValidation;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
@@ -12,12 +13,20 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var assembly = typeof(Program).Assembly;
 
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
-builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly, includeInternalTypes: true);
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+builder.Services.AddValidatorsFromAssembly(assembly, includeInternalTypes: true);
+builder.Services.AddMediatR(config => config.RegisterServicesFromAssembly(assembly));
+builder.Services.AddCarter();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
@@ -36,9 +45,9 @@ builder.Services.AddSwaggerGen(c =>
             Type = ReferenceType.SecurityScheme
         }
     };
-    
+
     c.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
-    
+
     c.AddSecurityRequirement(new OpenApiSecurityRequirement
     {
         {
@@ -47,15 +56,9 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 
-builder.Services.AddDbContext<StoreContext>(opt =>
-{
-    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+
 builder.Services.AddCors();
-builder.Services.AddIdentityCore<User>(opt =>
-    {
-        opt.User.RequireUniqueEmail = true;
-    })
+builder.Services.AddIdentityCore<User>(opt => { opt.User.RequireUniqueEmail = true; })
     .AddRoles<Role>()
     .AddEntityFrameworkStores<StoreContext>();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -67,8 +70,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ValidateAudience = false,
             ValidateLifetime = true,
             ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.
-                GetBytes(builder.Configuration["JWTSettings:TokenKey"]))
+            IssuerSigningKey =
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWTSettings:TokenKey"]))
         };
     });
 builder.Services.AddAuthorization();
@@ -84,24 +87,21 @@ app.UseMiddleware<ExceptionMiddleware>();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.ConfigObject.AdditionalItems.Add("persistAuthorization", true);
-    });
+    app.UseSwaggerUI(c => { c.ConfigObject.AdditionalItems.Add("persistAuthorization", true); });
 }
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
 
-app.UseCors(opt =>
-{
-    opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
-});
+app.UseCors(opt => { opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000"); });
+
 
 app.UseAuthentication();
 app.UseAuthorization();
 
+
+app.MapCarter();
 app.MapControllers();
 app.MapFallbackToController("Index", "Fallback");
 
@@ -110,7 +110,7 @@ var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
 var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
 var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
 try
-{   
+{
     await context.Database.MigrateAsync();
     await DbInitializer.Initialize(context, userManager);
 }
